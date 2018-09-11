62c5252aba446b7e730d820916bfc3cf4de8eb31
Surov Konstantin <surov_k_e@ntik.ru>
2018-09-11 14:43:54 +0300

    Start. Create signup form

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from '@angular/router';
+
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from "./auth/signup/signup.component";
+import { TrainingComponent } from "./training/training.component";
+import { LoginComponent } from "./auth/login/login.component";
+
+const routes: Routes = [
+    {path: '', component: WelcomeComponent},
+    {path: 'signup', component: SignupComponent},
+    {path: 'training', component: TrainingComponent},
+    {path: 'login', component: LoginComponent}
+];
+
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+
+export class AppRoutingModule {}
